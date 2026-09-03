52ba0bf011388ce5b80bb8b9f0554efa209bdf4c
Adeyemiadigun <adeyemiadigun12@gmail.com>
2026-09-03 22:33:25 +0100

    Add ListingPropertyCommandHandler and ListPropertyCommand

---
+++ RentBridge.Application/Command/Property/ListPropertyCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using RentBridge.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentBridge.Application.Command.Property
+{
+    public record class ListPropertyCommand(Guid PropertyId,
+    string Title,
+    decimal PriceAmount,
+    string? Description = null) : IRequest<Result<Guid>>;
+}
---
+++ RentBridge.Application/Command/Property/ListingCommandHandler.cs
@@ -0,0 +1,64 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using RentBridge.Application.Common.Interfaces;
+using RentBridge.Application.Common.Interfaces.Repositories;
+using RentBridge.Domain.Aggregates.Users;
+using RentBridge.Domain.Common;
+using RentBridge.Domain.ValueObjects;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentBridge.Application.Command.Property
+{
+    public class ListingPropertyCommandHandler(IUnitOfWork unitOfWork, ICurrentUser _currentUser, ILogger<ListingPropertyCommandHandler> logger) : IRequestHandler<ListPropertyCommand, Result<Guid>>
+    {
+        public async Task<Result<Guid>> Handle(ListPropertyCommand request, CancellationToken cancellationToken)
+        {
+            var userId = _currentUser.UserId;
+            if (userId is null)
+            {
+                logger.LogInformation("User not authenticated");
+                return Result<Guid>.Fail("User not Authenticated");
+            }
+
+            var user = await unitOfWork.Repository<User>().FirstOrDefault(u => u.Id == _currentUser.UserId, cancellationToken);
+            if (user == null)
+            {
+                logger.LogInformation("User not found");
+                return Result<Guid>.Fail("User not found");
+            }
+            if (!user.IdentityVerified)
+            {
+                logger.LogInformation("User identity not verified");
+                return Result<Guid>.Fail("User identity not verified");
+            }
+
+            var property = await unitOfWork.Repository<Domain.Aggregates.Property>().FirstOrDefault(p => p.Id == request.PropertyId, cancellationToken);
+            if(property == null)
+            {
+                logger.LogInformation("Property not found {propertyId}",request.PropertyId);
+                return Result<Guid>.Fail("Property not found");
+            }
+            if(property.OwnerUserId != user.Id)
+            {
+                logger.LogInformation("User {userId} is not the owner of property {propertyId}", user.Id, request.PropertyId);
+                return Result<Guid>.Fail("You can only list your own property");
+            }
+            if(!property.IsVerified)
+            {
+                logger.LogInformation("Property Has not been Verified");
+                return Result<Guid>.Fail("Property Has not been Verified");
+            }
+            var money = Money.Naira(request.PriceAmount);
+            var propertyListing = new Listing(userId.Value,property.Id,request.Title,money,request.Description);
+
+             unitOfWork.Repository<Listing>().Add(propertyListing);
+
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Result<Guid>.Ok(propertyListing.Id);
+        }
+    }
+}
